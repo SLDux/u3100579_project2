75058ea9910d1d10bae03b06b5126f5c4a308da2
SLDux <47551368+SLDux@users.noreply.github.com>
2019-04-24 01:21:04 +1000

    swaped out childthemes still flexia

---
+++ wp-content/themes/u3100579_project2/functions.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Flexia Child - Flexia
+ * @since 1.0.0
+ */
+
+/*
+ * Define Constants
+ */
+define( 'FLEXIA_CHILD_FLEXIA_CHILD_VERSION', '1.0.0' );
+
+/*
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+    $parent_style = 'parent-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), FLEXIA_CHILD_FLEXIA_CHILD_VERSION, 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
+
