ce3289db59ba21c438a31eb4bc3b97c0b982cb15
SLDux <47551368+SLDux@users.noreply.github.com>
2019-04-24 21:50:21 +1000

    trshootingtheme

--- wp-content/themes/u3100579_project2/functions.php
+++ wp-content/themes/u3100579_project2/functions.php
@@ -15,7 +15,7 @@ define( 'FLEXIA_CHILD_FLEXIA_CHILD_VERSION', '1.0.0' );
 /*
  * Enqueue styles
  */
-function child_enqueue_styles() {
+/* function child_enqueue_styles() {
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), FLEXIA_CHILD_FLEXIA_CHILD_VERSION, 'all' );
