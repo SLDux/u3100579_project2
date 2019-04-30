71615c3ab83af8d64a1efb3912add2b7da53ce41
SLDux <47551368+SLDux@users.noreply.github.com>
2019-04-30 23:43:56 +1000

    update to search.php for styling

--- wp-content/themes/u3100579_project2/searchform.php
+++ wp-content/themes/u3100579_project2/searchform.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template for displaying search forms in Twenty Seventeen
+ * Template for displaying search forms in child theme of Twenty Seventeen
  *
  * @package WordPress
  * @subpackage Twenty_Seventeen
